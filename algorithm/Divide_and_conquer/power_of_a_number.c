517e6464b01dc9ea445c0a3d47a15c210813d565
genius_stack <geniustsk@foxmail.com>
2019-05-20 07:55:39 -0700

    complete power_of_a_number.c

--- algorithm/Divide_and_conquer/power_of_a_number.c
+++ algorithm/Divide_and_conquer/power_of_a_number.c
@@ -5,10 +5,23 @@
 int multiply(int, int);
 
 int main(){
-
+	int num = 2;
+	int power = 10;
+	printf("%d\n", multiply(num, power));
 	return 0;
 }
 
 int multiply(int number, int power){
-
+	if(number == 0){
+		return 0;
+	}
+	if(power == 0){
+		return 1;
+	}
+	if(power % 2 == 0){
+		return multiply(number, power / 2) * multiply(number, power / 2);
+	}
+	else{
+		return multiply(number, (power - 1) / 2) * multiply(number, (power - 1) / 2) * number;
+	}
 }
